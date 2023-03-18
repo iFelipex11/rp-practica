90a1fbdf1740f7ff800377b8db50978adc4d794a
Felipe <felipeap1011@gmail.com>
2023-03-18 15:06:38 -0500

    Agregue la parte de modulos

---
+++ Cobjects/mdl1.mjs
@@ -0,0 +1,13 @@
+import {freezer} from "./nmdl2.mjs";
+
+
+const nevecon = new freezer({
+    color: "Gray",
+    weight: 100 + "Kg",
+    width: 92 + "cm",
+    height: 178 + "cm",
+    depth: 72 + "cm",
+})
+
+console.log(nevecon)
+
